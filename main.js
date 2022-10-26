4ed4f7709b6f3b6ab6e48a692fdc09ada0a640fe
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-10-26 13:25:21 -0300

    fix

--- main.js
+++ main.js
@@ -58,8 +58,6 @@ const scrollReveal = ScrollReveal({
 scrollReveal.reveal(
  `#home .image, #home .text,
   #about .image, #about .text,
-  #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links,
   footer .brand, footer .social
   `,
